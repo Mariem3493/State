7355922b8acf59f09cb2e004729b05dd745be1fe
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-07 21:38:17 +0200

    Update App.test.js

--- state/src/App.test.js
+++ state/src/App.test.js
@@ -1,6 +1,8 @@
+/*importation des fonctionnalités render et screen*/
 import { render, screen } from '@testing-library/react';
+/*importation du composant App*/
 import App from './App';
-
+/*Fonction pour rendre le composant App*/
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
