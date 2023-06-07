f0bed12f3feec5832be369aacc1216ac24dee4f4
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-07 21:24:12 +0200

    Update App.js

--- state/src/App.js
+++ state/src/App.js
@@ -1,5 +1,8 @@
+/*importation de la bibliothèque react et de la classe component*/
 import React, { Component } from 'react'
+/*importation du composant Bio*/
 import Bio from './Composant/Bio'
+/*Fonction pour initialiser l'état de composant à false*/
 export default class App extends Component {
 constructor(){
   super()
@@ -8,9 +11,11 @@ constructor(){
    
   }
 }
+  /*Changement de l'état du composant*/
 toggle=()=>{
   this.setState({show: !this.state.show})
 }
+  /*Fonction qui affiche le bouton show-hide*/
   render() {
     return (
       <div> 
